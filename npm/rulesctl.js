ef48ccdf6b28fc7c43772ee037effc6db4e82e11
choigawoon <choigawoon@gmail.com>
2025-04-18 03:14:30 +0900

    버전 업데이트: package.json 및 rulesctl.js에서 버전을 0.1.0에서 0.1.1로 변경하여 새로운 릴리즈 반영.

--- npm/rulesctl.js
+++ npm/rulesctl.js
@@ -9,7 +9,7 @@ const { execSync } = require("child_process");
 
 const OWNER = "choigawoon";
 const REPO = "rulesctl";
-const VERSION = "v0.1.0";
+const VERSION = "v0.1.1";
 
 async function downloadFile(url, dest) {
   console.log(`Downloading from: ${url}`);
